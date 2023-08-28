e54236e741e28e6970aac1995bcd5214e4e717ed
Chen Frederick <frederickfd88@gmail.com>
2023-08-28 23:37:17 +0700

    feat: add `views` directory files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CompositionApiView from "@/components/CompositionApiView.vue";
+
+// const onClick = (event: Event) => {
+const onClick = () => {
+  console.log("clicked");
+};
+</script>
+
+<template lang="pug">
+.home-container 
+  .header 
+    h1.header-title Welcome to Vue3 - Composition API Typescript Page
+    h3.header-subtitle.mt-2 By Frederick Chen
+
+    VBtn(variant="outlined" color="indigo-darken-2" href="https://github.com/Frederick-88/vue3-typescript-app" target="_blank").mt-5 Check Github Source Code
+  
+  hr.my-8
+
+  .content
+    CompositionApiView
+</template>
+
+<style lang="scss"></style>
---
+++ src/views/dep/OptionsApiView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="options-api__container">
+    <router-link class="nav" to="/">Back to Home</router-link>
+    <h1 class="title">Options API View</h1>
+
+    <div class="content">
+      <p>{{ name }} - {{ age }}</p>
+      <button @click="changeName('Zelda')">Change Name</button>
+      <button @click="changeAge(30)">Change Age</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import { mapStores } from "pinia";
+import { useGeneralStore } from "@/stores/general";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+      return age;
+    },
+  },
+  // computed: {
+  //   ...mapStores(useGeneralStore)
+  // },
+  // watch: {},
+  // onMounted(() => {
+  //   console.log(this.generalStore);
+  // })
+});
+</script>
+
+<style lang="scss">
+.options-api__container {
+  .title {
+    text-decoration: underline;
+  }
+}
+</style>
