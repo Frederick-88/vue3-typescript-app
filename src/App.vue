1502e04691b09d9c92f2fab627d184efdffc8ad4
Chen Frederick <frederickfd88@gmail.com>
2023-08-28 23:33:36 +0700

    feat: vue3 typescript app init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+main.app
+  section.page-container
+    RouterView
+</template>
+
+<style lang="scss">
+.app {
+  min-height: 100vh;
+  padding: 80px;
+}
+</style>
