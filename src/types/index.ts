3f23319ec90702ef403150d09e5baf1e41e30b01
Chen Frederick <frederickfd88@gmail.com>
2023-08-28 23:36:16 +0700

    feat: add reusable typescript interface storage

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+// -----
+// TS Data Types
+// -----
+// - Number
+// - String
+// - Boolean
+// - Null / Undefined
+// - Object
+// - Array > number[] or Array<number>
+// - Any
+// - Never > used for functions that returns error (e.g "throw New Error", etc)
+// - Etc that is not common (e.g unknown, etc)
+// -----
+
+// "Inteface"TodoObj variable naming
+interface ITodoObj {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export type { ITodoObj };
