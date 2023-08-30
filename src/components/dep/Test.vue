6ee7a6157a052c0b823d942fa03cf67c52752da8
Chen Frederick <frederickfd88@gmail.com>
2023-08-30 22:36:17 +0700

    feat: updated personal TS notes

--- src/components/dep/Test.vue
+++ src/components/dep/Test.vue
@@ -5,22 +5,48 @@ import { type ITodoObj } from "@/types/index";
 
 let testDefine: string | number = 1;
 let itemList: string[] = ["item-01", "item-02", "item-03"];
+let person: { name: string; age: number; working: boolean } = {
+  name: "John",
+  age: 21,
+  working: false,
+};
 
 // ---
-const props = defineProps({
-  selectedTodo: {
-    type: Object as PropType<ITodoObj>,
-    default: () => {},
-  },
-  isShow: {
-    type: Boolean,
-    default: false,
+export interface Props {
+  isShow: boolean;
+  selectedTodo: ITodoObj;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isShow: false,
+  selectedTodo: () => {
+    return {
+      userId: -1,
+      id: -1,
+      title: "",
+      description: "",
+      completed: false,
+    };
   },
 });
 
+// --- Optional Props Definition
+// const props = defineProps({
+//   selectedTodo: {
+//     type: Object as PropType<ITodoObj>,
+//     default: () => {},
+//   },
+//   isShow: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
 // ---
+
 const emit = defineEmits<{
-  (e: "click"): void;
+  // --- note: "?" so it become okay if we dont pass any value
+  // (e: "click", event: Event): void;
+  (event: "click", value?: string): void;
 }>();
 
 // ---
@@ -29,6 +55,7 @@ const numberAge = ref(25);
 const age = ref<string | number>(25);
 
 // ---
+// means should only return number
 const doubleAgeComputed = computed<number>(() => {
   return numberAge.value * 2;
 });
@@ -48,6 +75,13 @@ const doubleAgeWatch = ref<number>(0);
 watch(numberAge, (newVal) => {
   doubleAgeWatch.value = newVal * 2;
 });
+
+watch(
+  () => props.selectedTodo,
+  (newVal) => {
+    console.log(newVal, "new selected todo");
+  }
+);
 
 // ---
 onMounted(() => {});
