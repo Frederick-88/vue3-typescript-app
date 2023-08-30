f0760f9ea99e6ee1467f474f7034821d2094d2c4
Chen Frederick <frederickfd88@gmail.com>
2023-08-30 22:35:51 +0700

    feat: add vuetify modal

---
+++ src/components/children/Modal.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useGeneralStore } from "@/stores/general";
+
+const generalStore = useGeneralStore();
+
+export interface Props {
+  isShow: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+  isShow: false,
+});
+
+const emit = defineEmits<{
+  (event: "close"): void;
+}>();
+
+const modalModel = computed({
+  get() {
+    return props.isShow;
+  },
+  set(value) {
+    if (!value) {
+      emit("close");
+    }
+  },
+});
+
+const getTodoTitle = computed<string>(() => {
+  return `Todo Title: ${generalStore.selectedTodo.title}`;
+});
+</script>
+
+<template>
+  <v-dialog
+    v-model="modalModel"
+    transition="dialog-bottom-transition"
+    width="600px"
+    maxWidth="90%"
+  >
+    <v-card>
+      <v-toolbar color="indigo-darken-2" :title="getTodoTitle"></v-toolbar>
+      <v-card-text class="pa-12">
+        <h6 class="text-h6 font-weight-bold">Details</h6>
+        <hr class="my-5" />
+        <p class="mb-2">Id: {{ generalStore.selectedTodo.id }}</p>
+        <p class="mb-2">UserId: {{ generalStore.selectedTodo.userId }}</p>
+        <p>Description: {{ generalStore.selectedTodo.description }}</p>
+      </v-card-text>
+      <v-card-actions class="justify-end">
+        <v-btn variant="text" @click="emit('close')">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped></style>
